fcc46df61fc94ee259287858f380bb5310df148a
lotusjoshigehu <joshikamal626@gmail.com>
2025-06-30 13:38:16 +0530

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages,setup
+
+setup(
+    name="Generative ai project",
+    version="0.0.0",
+    author="Kamal Joshi",
+    author_email="joshikamal626@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
